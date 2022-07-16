1f50901c5683ca1a69f898f21fd023b03176e793
Kiragu <samuelkiragu107@gmail.com>
2022-07-16 18:38:12 +0300

    Formatted code for readability

--- scripts/script.js
+++ scripts/script.js
@@ -39,16 +39,14 @@ function playRound(playerSelection, computerSelection){
         return "\tIt is a Tie! You both Win and Lose";
     }
 
+    //Actual game logic
     if(playerSelection == "ROCK" && computerSelection == "SCISSORS"){
         playerWins = true;
-    }
-    else if(playerSelection == "PAPER" && computerSelection == "ROCK"){
+    }else if(playerSelection == "PAPER" && computerSelection == "ROCK"){
         playerWins = true;
-    }
-    else if(playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+    }else if(playerSelection == "SCISSORS" && computerSelection == "PAPER"){
         playerWins = true;
-    }
-    else{
+    }else{
         playerWins = false;
     }
 
@@ -62,7 +60,7 @@ function playRound(playerSelection, computerSelection){
 function game(){
     let playerSelection = null;
     let computerSelection = null;
-    let result = null
+    let result = null;
     for (let i = 0; i < 5; i++) {
         playerSelection = prompt("Rock, Paper, or Scissors?");
         computerSelection = computerPlay();
