ecfd41de6255465522ace8e40d3a3da1842f5881
Kiragu <samuelkiragu107@gmail.com>
2022-07-22 15:38:59 +0300

    Added separate function to render results. Refactored game logic Refactored page structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
-let round = 1;
+const ELEMENT_SELECTORS = [
+    "#PlayerChoice","#ComputerChoice","#Results","#PlayerScore","#ComputerScore"
+];
 
+//game variables
+let roundsPlayed = 0;
+let playerScore = 0;
+let compScore = 0;
+let drawScore = 0;
+let playerSelection = null;
+let computerSelection = null;
+let result = null;
+let winner;
 
 // function to get computer's choice
 function computerPlay() {
@@ -29,15 +40,44 @@ function playRound(playerSelection, computerSelection) {
     return `${playerWins ? 'WIN' : 'LOSE'}!`;
 }
 
-// Add eventListeners
+function renderData(){
+    document.querySelector("#RemRounds").textContent = 5 -roundsPlayed;
+    document.querySelector("#PlayerChoice").textContent = playerSelection;
+    document.querySelector("#ComputerChoice").textContent = computerSelection;
+    document.querySelector("#Results").textContent = result;
+    document.querySelector("#PlayerScore").textContent = playerScore;
+    document.querySelector("#ComputerScore").textContent = compScore;
+    document.querySelector("#DrawScore").textContent = drawScore;
+    if(roundsPlayed == 5) document.querySelector("#Winner").textContent = winner;
+}
+
 let buttons = document.querySelectorAll('button');
 buttons.forEach((btn, key) => {
     btn.addEventListener('click', () => {
-        // These are the game options
+        // Listening for Game Options
         if (key < 3){
             playerSelection = CHOICES[key];
             computerSelection = computerPlay();
             result = playRound(playerSelection, computerSelection);
+
+            if(result == "WIN!") ++playerScore;
+            if(result == "LOSE!") ++compScore;
+            if(result == "TIE") ++drawScore;
+            ++roundsPlayed;
+        }else{
+            roundsPlayed = 0;
+            playerScore = 0;
+            compScore = 0;
+            drawScore = 0;
+            playerSelection = "";
+            computerSelection = "";
+            result = "";
         }
+        if(roundsPlayed == 5){
+            winner = playerScore > compScore ? "PLAYER WINS" : "COMPUTER WINS";
+            // add functionality to disable play buttons
+            // add functionality to enable reset button
+        }
+        renderData();
     });
 });
